f529eed90147672038c082a21015480524a18cb6
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-06 15:45:37 -0300

    feat: add SearchClient to fetch data from elastic

---
+++ src/client/search.ts
@@ -0,0 +1,55 @@
+import elasticsearch from "elasticsearch";
+import { ELASTIC_URL } from "../config/constants";
+
+type SearchArgs = {
+  q: string;
+  artifact: string;
+  from?: number;
+  size?: number;
+};
+
+class SearchClient {
+  protected elasticClient: elasticsearch.Client;
+  protected artifactMap: Map<string, string[]>;
+
+  constructor() {
+    this.elasticClient = new elasticsearch.Client({
+      host: ELASTIC_URL,
+      log: "trace",
+    });
+    this.artifactMap = new Map([
+      [
+        "GENERAL",
+        [
+          "JURISPRUDENCIA",
+          "SUMULA",
+          "MODELOS",
+          "PECAS",
+          "ARTIGOS",
+          "NOTICIAS",
+          "LEGISLACAO",
+        ],
+      ],
+      ["JURISPRUDENCIA", ["JURISPRUDENCIA"]],
+    ]);
+  }
+
+  async search({ artifact, q, from, size }: SearchArgs) {
+    const data = await this.elasticClient.search({
+      index: this.artifactMap.get(artifact),
+      from,
+      size,
+      body: {
+        query: {
+          match: {
+            body: q,
+          },
+        },
+      },
+    });
+
+    return data;
+  }
+}
+
+export default new SearchClient();
