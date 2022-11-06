b717035d29021533cd387715b42259162f74ac47
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-06 14:11:17 -0300

    feat: add SearchResult type

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+import { gql } from 'apollo-server'
+
+export const typeDefs = gql`
+  extend schema
+    @link(
+      url: "https://specs.apollo.dev/federation/v2.0"
+    )
+
+  type SearchResult {
+    id: ID!
+    documentId: ID!
+    title: String!
+    artifact: String!
+    body: String
+    createdAt: String
+    relatedDocument: Document
+  }
+
+  type Query {
+    search: [SearchResult]
+  }
+`
