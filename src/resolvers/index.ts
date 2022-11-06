16ab626a2e9bb8447a42c40ad56e8c4d7f637c29
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-06 15:46:00 -0300

    feat: use SearchClient to retrieve data from elastic

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,17 +1,19 @@
+import searchClient from "../client/search";
+
 export const resolvers = {
   SearchResult: {
     relatedDocument(searchResult) {
       return {
         // Allow data fetch from document subgraph.
         // This will trigger __resolveReference resolver in Document type in document domain graph service
-        __typename: 'Document',
-        id: searchResult.documentId
-      }
-    }
-  },
-  Query: {
-    search: async () => {
-      // TODO trigger query in elasticsearch
+        __typename: "Document",
+        id: searchResult.documentId,
+      };
     },
   },
-}
+  Query: {
+    search: (_, args) => {
+      return searchClient.search(args);
+    },
+  },
+};
