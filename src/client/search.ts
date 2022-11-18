25ff61173a83411567bf7e68332f201d02b46871
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-18 14:43:45 -0300

    chore: ingest data with logstash

--- src/client/search.ts
+++ src/client/search.ts
@@ -50,21 +50,6 @@ class SearchClient {
 
     return (data?.hits?.hits || []).map((res) => res._source)
   }
-
-  async index() {
-    await this.elasticClient.index({
-      index: 'jurisprudencia',
-      type: 'document',
-      body: {
-        id: '1',
-        documentId: '6365742a63431fce0dc88e4e',
-        title:
-          'Supremo Tribunal Federal STF - RECURSO EXTRAORDINÁRIO: RE 600091 MG',
-        artifact: 'JURISPRUDENCIA',
-        body: 'EMENTA Recurso extraordinário – Competência – Processual Civil e do Trabalho – Repercussão geral reconhecida – Ação de indenização decorrente de danos sofridos em acidente de trabalho – Demanda diretamente decorrente de relação de trabalho, sendo irrelevante, para fins de fixação da competência, o fato de ter sido ajuizada por sucessores do trabalhador falecido – Aplicação da norma do art. 114, inciso VI, da Constituição Federal, com a redação que a ela foi dada pela Emenda Constitucional nº 45/04 – Reconhecimento da competência da Justiça Federal do Trabalho para o processamento do feito – Recurso não provido.'
-      }
-    })
-  }
 }
 
 export default new SearchClient()
